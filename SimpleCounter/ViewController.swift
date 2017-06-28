becedc811ae145c7d6fb5b1ce20b6cbeeadc21ba
Blake Merryman <blake@blackpixel.com>
2017-06-28 16:19:58 -0500

    Initial Commit

---
+++ SimpleCounter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleCounter
+//
+//  Created by Blake Merryman on 6/28/17.
+//  Copyright © 2017 Blake Merryman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
